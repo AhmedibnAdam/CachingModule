3224e24dc08c0502566c9c9ca4e44e050ded1328
AhmedibnAdam <ibn.abuadam@gmail.com>
2025-02-06 21:26:55 +0200

    Improved Naming & Structure

---
+++ Sources/CachingModule/Protocols/SecureCaching.swift
@@ -0,0 +1,21 @@
+//
+//  SecureCaching.swift
+//  CachingModule
+//
+//  Created by Ahmad on 05/02/2025.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+
+public protocol SecureCaching {
+    func set(_ value: String, forKey key: String) async throws
+    func get(forKey key: String) async throws -> String?
+    func removeValue(forKey key: String) async throws
+}
+
+
+
+
+
